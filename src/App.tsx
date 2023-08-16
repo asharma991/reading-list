3b0d114fbce554fd228cdae0d416f6e2528054e5
Abhishek Sharma <asharma991@outlook.com>
2023-08-17 02:19:42 +0530

    removed unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
 import './styles.css';
-import Img from './web-api.png';
-import Logo from './react-icon.svg';
-import ClickCounter from './ClickCounter';
 
 export const App = () => {
-  const name = 'abhishek';
-  return (
-    <>
-      <h1>
-        React TypeScript Webpack Starter Template - {name} {process.env.name}
-      </h1>
-      ;
-      <img src={Img} alt='Web API' width='300' height='200' />
-      <img src={Logo} alt='React Logo' width='300' />
-      <ClickCounter />
-    </>
-  );
+  return <h1>Reading List</h1>;
 };
